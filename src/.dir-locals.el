49a06110cd5e0cfeda2d4e7712e88ffeb8369feb
Panadestein <rpana92@gmail.com>
2025-02-25 13:04:14 +0100

    refact: streamline new post setup.

--- src/.dir-locals.el
+++ src/.dir-locals.el
@@ -1,4 +1,4 @@
 ;;  Directory local variables
 ;;  Makes org-mode files with BQN code look better in Emacs
 
-((org-mode . ((face-remapping-alist . ((default (:family "BQN386 Unicode" :height 180)))))))
+((org-mode . ((face-remapping-alist . ((default (:family "BQN386 Unicode" :height 200)))))))
